1e10bf285b6e10b985881b0d1818d9261e0c1580
cleverche <chenche0713@126.com>
2017-11-15 17:27:30 +0800

    SpringBoot AngularJs1.x Study

---
+++ src/main/java/com/uemao/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.uemao.service.dto;
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('sBootApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
